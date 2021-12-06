e3409dcd4316951c4c41eda0d6708860b5de5116
Weslley Carneiro <weslley@outlook.com>
2021-12-06 20:10:55 -0300

    - construtor base do contexto - version bump

--- src/Optsol.Components.Repository.Infra.EntityFrameworkCore/Contexts/Context.cs
+++ src/Optsol.Components.Repository.Infra.EntityFrameworkCore/Contexts/Context.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Optsol.Components.Repository.Infra.EntityFrameworkCore.Contexts
 {
-    public class Context: DbContext
+    public class Context : DbContext
     {
+        public Context() : base() { }
 
+        public Context([NotNull] DbContextOptions options) : base(options) { }
     }
 }
